6dadad0ebca79fcaeebf7b6c30ff59c477cbf8fb
priyesshrai <priyeshrai369@gmail.com>
2026-03-14 13:54:15 +0530

    feat: enhance DesktopHeader with submenu support and scroll-based visibility for action buttons

--- src/components/header/desktop.header.tsx
+++ src/components/header/desktop.header.tsx
@@ -1,97 +1,135 @@
 'use client';
 import { MenuItems } from '@/constant/menu';
-import { ChevronDown, ChevronRight } from 'lucide-react';
-import { AnimatePresence, motion } from 'motion/react';
+import { ChevronDown, ChevronRight, Heart, ShoppingCart, User } from 'lucide-react';
+import { AnimatePresence, motion, useMotionValueEvent, useScroll } from 'motion/react';
 import Link from 'next/link';
 import { useState } from 'react';
 
 export default function DesktopHeader() {
   const [active, setActive] = useState<number | null>(null);
+  const { scrollY } = useScroll();
+  const [scrolled, setScrolled] = useState<boolean | null>(false);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    if (latest > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(null);
+    }
+  });
 
   return (
     <div className='sticky top-0 z-50 w-full bg-primary-100 max-md:hidden lg:px-12 md:px-10 px-8 h-12'>
-      <div className='w-full max-w-7xl mx-auto flex items-center h-full'>
-        {
-          MenuItems.map((item, idx) => (
-            <div
-              key={idx}
-              className='h-full relative px-5 group flex items-center text-sm font-medium text-primary-500'
-              onMouseEnter={() => setActive(idx)}
-              onMouseLeave={() => setActive(null)}
-            >
+      <div className='w-full max-w-7xl mx-auto flex items-center h-full justify-between'>
 
-              {item.path ? (
-                <Link
-                  href={item.path}
-                  className='h-full flex items-center'
-                >
-                  {item.name}
-                </Link>
-              ) : (
-                <span className="cursor-pointer flex items-center gap-2.5 h-full">
-                  {item.name}
-                  <ChevronDown
-                    size={12}
-                    className={`transition-transform duration-300 ${active === idx ? "rotate-180" : ""}`}
-                  />
-                </span>
-              )}
+        <div className='flex items-center h-full'>
+          {
+            MenuItems.map((item, idx) => (
+              <div
+                key={idx}
+                className='h-full relative px-5 group flex items-center text-sm font-medium text-primary-500'
+                onMouseEnter={() => setActive(idx)}
+                onMouseLeave={() => setActive(null)}
+              >
 
-
-              <AnimatePresence>
-                {active === idx && item.subMenu && (
-                  <motion.div
-                    initial={{ opacity: 0, y: 10, scale: 0.95 }}
-                    animate={{ opacity: 1, y: 0, scale: 1 }}
-                    exit={{ opacity: 0, y: 10, scale: 0.95 }}
-                    transition={{ duration: 0.2, ease: "easeOut" }}
-                    className="absolute top-full left-0 bg-white shadow-md w-56 rounded-md overflow-hidden"
+                {item.path ? (
+                  <Link
+                    href={item.path}
+                    className='h-full flex items-center'
                   >
-                    {item.subMenu.map((sub, j) => (
-                      <Link
-                        key={j}
-                        href={sub.path || "#"}
-                        className="px-4 py-2 flex items-center gap-1.5 hover:text-primary-500 transition-colors duration-300 text-zinc-600 text-sm group/sub hover:bg-primary-50"
-                      >
-                        <ChevronRight
-                          size={10}
-                          className='group-hover/sub:translate-x-0.5 transition-transform duration-300'
-                        />
-                        {sub.name}
-                      </Link>
-                    ))}
-                  </motion.div>
+                    {item.name}
+                  </Link>
+                ) : (
+                  <span className="cursor-pointer flex items-center gap-2.5 h-full">
+                    {item.name}
+                    <ChevronDown
+                      size={12}
+                      className={`transition-transform duration-300 ${active === idx ? "rotate-180" : ""}`}
+                    />
+                  </span>
                 )}
-              </AnimatePresence>
 
-              <AnimatePresence mode='popLayout'>
-                {active === idx && (
-                  <motion.span
-                    layoutId="nav-hover"
-                    className="absolute inset-x-0 bottom-0 bg-primary-600 h-0.5"
-                    initial={{
-                      opacity: 0,
-                      scale: 0.97,
-                    }}
-                    animate={{
-                      opacity: 1,
-                      scale: 1,
-                    }}
-                    exit={{
-                      opacity: 0,
-                      scale: 0.98,
-                    }}
-                    transition={{
-                      type: "tween",
-                      ease: [0.22, 1, 0.36, 1],
-                      duration: 0.5,
-                    }}
-                  />
-                )}
-              </AnimatePresence>
-            </div>
-          ))
-        }
+
+                <AnimatePresence>
+                  {active === idx && item.subMenu && (
+                    <motion.div
+                      initial={{ opacity: 0, y: 10, scale: 0.95 }}
+                      animate={{ opacity: 1, y: 0, scale: 1 }}
+                      exit={{ opacity: 0, y: 10, scale: 0.95 }}
+                      transition={{ duration: 0.2, ease: "easeOut" }}
+                      className="absolute top-full left-0 bg-white shadow-md w-56 rounded-md overflow-hidden"
+                    >
+                      {item.subMenu.map((sub, j) => (
+                        <Link
+                          key={j}
+                          href={sub.path || "#"}
+                          className="px-4 py-2 flex items-center gap-1.5 hover:text-primary-500 transition-colors duration-300 text-zinc-600 text-sm group/sub hover:bg-primary-50"
+                        >
+                          <ChevronRight
+                            size={10}
+                            className='group-hover/sub:translate-x-0.5 transition-transform duration-300'
+                          />
+                          {sub.name}
+                        </Link>
+                      ))}
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+
+                <AnimatePresence mode='popLayout'>
+                  {active === idx && (
+                    <motion.span
+                      layoutId="nav-hover"
+                      className="absolute inset-x-0 bottom-0 bg-primary-600 h-0.5"
+                      initial={{
+                        opacity: 0,
+                        scale: 0.97,
+                      }}
+                      animate={{
+                        opacity: 1,
+                        scale: 1,
+                      }}
+                      exit={{
+                        opacity: 0,
+                        scale: 0.98,
+                      }}
+                      transition={{
+                        type: "tween",
+                        ease: [0.22, 1, 0.36, 1],
+                        duration: 0.5,
+                      }}
+                    />
+                  )}
+                </AnimatePresence>
+              </div>
+            ))
+          }
+        </div>
+
+        <AnimatePresence mode='wait'>
+          {
+            scrolled && (
+              <motion.div className='flex items-center gap-3'
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 1 }}
+                exit={{ opacity: 0, y: -50 }}
+                transition={{ ease: 'easeInOut', duration: 0.2 }}
+              >
+                <button className="relative hidden md:flex items-center justify-center rounded-sm transition-colors duration-300 ease-in-out hover:bg-primary-400 cursor-pointer">
+                  <Heart className="text-primary-500" size={28} strokeWidth={1} fill='#ff710a' />
+                </button>
+
+                <button className="relative hidden md:flex items-center justify-center rounded-sm transition-colors duration-300 ease-in-out hover:bg-primary-400 cursor-pointer">
+                  <ShoppingCart className="text-primary-500" size={28} strokeWidth={1} fill='#ff710a' />
+                </button>
+
+                <button className="hidden md:flex items-center justify-center rounded-sm transition-colors duration-300 ease-in-out hover:bg-primary-400 cursor-pointer">
+                  <User className="text-primary-500" size={28} strokeWidth={1} fill='#ff710a' />
+                </button>
+              </motion.div>
+            )
+          }
+        </AnimatePresence>
       </div>
     </div>
   )
