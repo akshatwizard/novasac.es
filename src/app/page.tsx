9341b31771922002574f9909dd91978a3a5dc2c3
akshatwizard <akshat@wizards.co.in>
2026-04-11 14:41:27 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
       <Products />
       <Industry />
       <BentoGrid />
-      <TrendingProducts />
+      
       <WhyChooseUs />
       <Testimonials />
       <Promotional />
