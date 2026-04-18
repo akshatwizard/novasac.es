fadcf04e774c4d5cec39c8eb90b4ac6e868cddb9
priyesshrai <priyeshrai369@gmail.com>
2026-04-18 13:17:33 +0530

    add mobile strip

--- src/components/ui/section.tsx
+++ src/components/ui/section.tsx
@@ -8,7 +8,7 @@ type Props = ComponentProps<"section"> & {
 export default function Section({ children, className, ...rest }: Props) {
     return (
         <section
-            className={cn("w-full lg:px-12 md:px-10 px-6", className)}
+            className={cn("w-full lg:px-12 md:px-10 px-4", className)}
             {...rest}
         >
             {children}
