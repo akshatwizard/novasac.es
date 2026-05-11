7eb503f5661575dcfd93ee93ae0008368bc75c1d
priyesshrai <priyeshrai369@gmail.com>
2026-05-11 11:59:44 +0530

    feat: enhance layout responsiveness in About and AdditionalDetails components

--- src/components/additional_details.tsx
+++ src/components/additional_details.tsx
@@ -37,7 +37,7 @@ export default function AdditionalDetails() {
     return (
         <Section>
             <Wrapper>
-                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6'>
 
                     {details.map((item, idx) => (
                         <div
