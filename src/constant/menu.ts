43ca5a8a011affd3b8a52a0055227b1c6d23538a
akshatwizard <akshat@wizards.co.in>
2026-08-30 00:52:37 +0530

    Update menu.ts

--- src/constant/menu.ts
+++ src/constant/menu.ts
@@ -3,31 +3,29 @@ export type MenuType = {
     path: string;
     icon: string
 };
-
 type SubMenu = Omit<MenuType, "subMenu">;
-
 export const MenuItems: MenuType[] = [
     // {
     //     name: "Home",
     //     path: "/",
     // },
     {
-        name: "Recycled Bags",
+        name: "Bolsas Recicladas",
         path: "/recycled-bags",
         icon: "/images/recycled/recycled.jpeg"
     },
     {
-        name: "Technical Textiles",
+        name: "Textiles Técnicos",
         path: "/technical-textiles",
         icon: "/images/textiles/tarpaulin.jpg"
     },
     {
-        name: "Custom-made Bulk Bags",
+        name: "Bolsas a Granel Personalizadas",
         path: "/custom-made-bags",
         icon: "/images/custom-bag/bag.jpg"
     },
     {
-        name: "About Us",
+        name: "Sobre Nosotros",
         path: "/about",
         icon: "/images/hero/company.jpeg"
     },
