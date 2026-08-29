8eba1a279853e54d2fe476e53b3e18f028f7b6b7
akshatwizard <akshat@wizards.co.in>
2026-08-29 19:10:26 +0530

    Update hero_carousel.tsx

--- src/components/hero/hero_carousel.tsx
+++ src/components/hero/hero_carousel.tsx
@@ -60,7 +60,7 @@ export default function HeroBanner() {
     const { data, isLoading } = useQuery<BannerData[]>({
         queryKey: ["hero_banner"],
         queryFn: async () => {
-            const res = await axios.get<BannerResponse>("https://www.gangapapers.in/novasac/api/home/banner");
+            const res = await axios.get<BannerResponse>("https://admin.novasac.es/api/home/banner");
             return res.data.data;
         },
     });
