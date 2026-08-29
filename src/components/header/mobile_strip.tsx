89e04a70482f5adf011572ed85351f582d56d001
akshatwizard <akshat@wizards.co.in>
2026-08-29 19:47:37 +0530

    Update mobile_strip.tsx

--- src/components/header/mobile_strip.tsx
+++ src/components/header/mobile_strip.tsx
@@ -1,32 +1,24 @@
 'use client';
-
 import { Heart, Home, ShoppingCart, User } from "lucide-react";
 import Link from "next/link";
 import { motion, useMotionValueEvent, useScroll } from 'motion/react'
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/context/auth_context";
 import { Dispatch, SetStateAction, useState } from "react";
-
 type Props = {
     openLoginModal: Dispatch<SetStateAction<boolean>>
 }
-
 export default function MobileStrip({ openLoginModal }: Props) {
     const router = useRouter();
     const { isAuthenticated, user } = useAuth();
     const { scrollY } = useScroll();
     const [scrollDirection, setScrollDirection] = useState("up")
-
     useMotionValueEvent(scrollY, "change", (current) => {
         const previous = scrollY.getPrevious() ?? 0;
         const diff = current - previous;
-
         if (Math.abs(diff) < 2) return;
-
         setScrollDirection(diff > 0 ? "down" : "up");
     });
-
-
     return (
         <motion.div
             initial={{ y: 0 }}
@@ -40,23 +32,23 @@ export default function MobileStrip({ openLoginModal }: Props) {
             }}
             className='md:hidden fixed inset-x-0 -bottom-1 h-16 w-full z-40'>
             <div className="w-full h-full bg-primary-600 grid grid-cols-4">
-                <Link aria-label="Home" href={'/'} className="w-full h-full flex items-center justify-center hover:bg-primary-500 transition-colors duration-300 ease-linear">
+                <Link aria-label="Inicio" href={'/'} className="w-full h-full flex items-center justify-center hover:bg-primary-500 transition-colors duration-300 ease-linear">
                     <Home fill="white" className="text-white" size={28} strokeWidth={1.8} />
-                    <span className="sr-only">Home</span>
+                    <span className="sr-only">Inicio</span>
                 </Link>
-                <Link aria-label="Home" href={'/'} className="w-full h-full flex items-center justify-center hover:bg-primary-500 transition-colors duration-300 ease-linear">
+                <Link aria-label="Inicio" href={'/'} className="w-full h-full flex items-center justify-center hover:bg-primary-500 transition-colors duration-300 ease-linear">
                     <Heart fill="white" className="text-white" size={28} strokeWidth={1.8} />
-                    <span className="sr-only">Home</span>
+                    <span className="sr-only">Inicio</span>
                 </Link>
-                <Link aria-label="Shopping Cart" href={'/'} className="w-full h-full flex items-center justify-center hover:bg-primary-500 transition-colors duration-300 ease-linear">
+                <Link aria-label="Carrito de compra" href={'/'} className="w-full h-full flex items-center justify-center hover:bg-primary-500 transition-colors duration-300 ease-linear">
                     <ShoppingCart fill="white" className="text-white" size={28} strokeWidth={1.8} />
-                    <span className="sr-only">Shopping Cart</span>
+                    <span className="sr-only">Carrito de compra</span>
                 </Link>
-                <button aria-label="Account/Profile" className="w-full h-full flex items-center justify-center hover:bg-primary-500 transition-colors duration-300 ease-linear cursor-pointer"
+                <button aria-label="Cuenta/Perfil" className="w-full h-full flex items-center justify-center hover:bg-primary-500 transition-colors duration-300 ease-linear cursor-pointer"
                     onClick={() => isAuthenticated ? router.push(`/profile/${user?.customer_id}`) : openLoginModal(true)}
                 >
                     <User fill="white" className="text-white" size={28} strokeWidth={1.8} />
-                    <span className="sr-only">Account/Profile</span>
+                    <span className="sr-only">Cuenta/Perfil</span>
                 </button>
             </div>
         </motion.div>
