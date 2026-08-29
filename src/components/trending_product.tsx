101effee243cc4fa3ca680ea6d24ff5a32d302d0
akshatwizard <akshat@wizards.co.in>
2026-08-29 18:40:04 +0530

    Update trending_product.tsx

--- src/components/trending_product.tsx
+++ src/components/trending_product.tsx
@@ -16,7 +16,7 @@ export default function TrendingProducts() {
         queryKey: ["trending_products"],
         queryFn: async () => {
             const res = await axios.get<ProductResponse>(
-                "https://www.gangapapers.in/novasac/api/home/trending-products"
+                "https://admin.novasac.es/api/home/trending-products"
             );
             return res.data.data;
         },
