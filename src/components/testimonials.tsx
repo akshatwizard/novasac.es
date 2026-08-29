08cd4f954dd93d1514b9b6fa2bca2ec7281b70dd
akshatwizard <akshat@wizards.co.in>
2026-08-29 18:39:39 +0530

    Update testimonials.tsx

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -30,7 +30,7 @@ export default function Testimonials() {
         queryKey: ['testimonial'],
         queryFn: async () => {
             const res = await axios.get<TestimonialResponse>(
-                "https://www.gangapapers.in/novasac/api/home/testimonials"
+                "https://admin.novasac.es/api/home/testimonials"
             );
             return res.data.data;
         },
