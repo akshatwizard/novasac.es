1618cd2d22d8d6719bce59ab7b95b3cdc041a2ae
akshatwizard <akshat@wizards.co.in>
2026-08-29 19:16:15 +0530

    Update page.tsx

--- src/app/profile/[user_id]/page.tsx
+++ src/app/profile/[user_id]/page.tsx
@@ -110,7 +110,7 @@ export default function Profile() {
   const { data: profile, isLoading, isError, error } = useQuery<ProfileData>({
     queryKey: ['customer-profile'],
     queryFn: async () => {
-      const res = await axios.get('https://www.gangapapers.in/novasac/api/customer/profile', {
+      const res = await axios.get('https://admin.novasac.es/api/customer/profile', {
         headers: {
           Authorization: `Bearer ${token}`,
           Accept: 'application/json',
