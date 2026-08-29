dff33c655cc622bd17e21490e737c5fd7cfaea41
akshatwizard <akshat@wizards.co.in>
2026-08-29 19:14:47 +0530

    Update catalog.ts

--- src/hooks/catalog.ts
+++ src/hooks/catalog.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { CatalogApiResponse } from "../types/catalog.types";
 
-const BASE_URL = "https://www.gangapapers.in/novasac/api";
+const BASE_URL = "https://admin.novasac.es/api";
 
 export interface CatalogFetchParams {
   slug: string[];
