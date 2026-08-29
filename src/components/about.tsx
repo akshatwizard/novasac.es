ae79d82a69d1d842dffdaf148ca2cf5383a9a9b2
akshatwizard <akshat@wizards.co.in>
2026-08-29 19:52:52 +0530

    Update about.tsx

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -3,7 +3,6 @@ import Section from './ui/section'
 import Wrapper from './ui/wrapper'
 import Link from 'next/link'
 import { ChevronRight } from 'lucide-react'
-
 export default function About() {
     return (
         <Section>
@@ -11,17 +10,16 @@ export default function About() {
                 <div className='w-full grid grid-cols-1 lg:grid-cols-[500px_1fr] gap-10 md:gap-4'>
                     <div className='w-full h-full'>
                         <h1 className='lg:text-6xl md:text-5xl text-3xl leading-tight font-light text-zinc-900'>
-                            Your partner <br /> in <em className='text-primary-500'>packaging solutions</em> <br />since 1980
+                            Tu socio <br /> en <em className='text-primary-500'>soluciones de embalaje</em> <br />desde 1980
                         </h1>
                     </div>
-
                     <div className='w-full h-full grid place-content-center'>
                         <p className='text-zinc-700 font-medium lg:text-lg md:text-base text-sm'>
-                            NOVASAC delivers bags built on strength, reliability, and sustainability. We carefully select quality materials to create packaging solutions you can trust, while staying committed to a greener future.
+                            NOVASAC ofrece bolsas fabricadas con resistencia, fiabilidad y sostenibilidad. Seleccionamos cuidadosamente materiales de calidad para crear soluciones de embalaje en las que puedes confiar, comprometidos siempre con un futuro más verde.
                             <br />
-                            More than a manufacturer, we are a partner — combining craftsmanship, innovation, and a people-first approach to support your projects and grow together.
+                            Más que un fabricante, somos un socio — combinando artesanía, innovación y un enfoque centrado en las personas para apoyar tus proyectos y crecer juntos.
                             <br />
-                            We are NOVASAC, and we look forward to welcoming you to our family.
+                            Somos NOVASAC, y esperamos darte la bienvenida a nuestra familia.
                         </p>
                         <Link
                             href={"/about"}
@@ -29,7 +27,7 @@ export default function About() {
                         >
                             <ChevronRight size={18} strokeWidth={3} />
                             <span>
-                                About Us
+                                Sobre Nosotros
                             </span>
                         </Link>
                     </div>
